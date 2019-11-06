964a829c0238968f17cf2f78f18429beec6dbeec
wxfnyu <wxf201@nyu.edu>
2019-11-05 20:42:48 -0500

    hopefully fixed it...

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,10 +18,11 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin hello --target $TARGET --release -- -C lto
+    cross rustc --bin magiarecord_automatic_updater --target $TARGET --release -- -C lto
 
     # TODO Update this to package the right artifacts
-    cp target/$TARGET/release/hello $stage/
+    cp target/$TARGET/release/magiarecord_automatic_updater $stage/
+    cp -r scripts_for_NA_install $stage
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
