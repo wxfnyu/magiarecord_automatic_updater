02631968891c29069234ac0be4b960c1cfcfc743
wxfnyu <wxf201@nyu.edu>
2019-11-06 00:49:03 -0500

    biggus reeussssssssss

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -22,8 +22,13 @@ main() {
     # TODO Update this to build the artifacts that matter to you
     cross rustc --bin magiarecord_automatic_updater --target $TARGET --release -- -C lto
 
+    wintarget="x86_64-pc-windows-gnu"
     # TODO Update this to package the right artifacts
-    cp {target/$TARGET/release/magiarecord_automatic_updater,target/$TARGET/release/magiarecord_automatic_updater*exe}  $stage/
+    if [$wintarget == $TARGET]
+        cp target/$TARGET/release/magiarecord_automatic_updater*exe $stage/
+    else
+        cp target/$TARGET/release/magiarecord_automatic_updater $stage/
+    fi
     cp -r scripts_for_NA_install $stage
 
     cd $stage
