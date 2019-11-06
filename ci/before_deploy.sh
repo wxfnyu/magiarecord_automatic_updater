fcc26ca8f55ea17f60bfe41676bc69cca7412ad8
wxfnyu <wxf201@nyu.edu>
2019-11-06 02:01:35 -0500

    switching to zip

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,22 +18,23 @@ main() {
     esac
 
     test -f Cargo.lock || cargo generate-lockfile
+    mkdir $stage/magiarecord
 
     # TODO Update this to build the artifacts that matter to you
     cross rustc --bin magiarecord_automatic_updater --target $TARGET --release -- -C lto
-
+    
     
     # TODO Update this to package the right artifacts
     if [[ $TARGET == *windows* ]]; then
         echo "targeting windows..."
-        cp target/$TARGET/release/magiarecord_automatic_updater*exe $stage/
+        cp target/$TARGET/release/magiarecord_automatic_updater.exe $stage/
     else
         cp target/$TARGET/release/magiarecord_automatic_updater $stage/
     fi
     cp -r scripts_for_NA_install $stage
 
     cd $stage
-    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    7z a $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
     cd $src
 
     rm -rf $stage
